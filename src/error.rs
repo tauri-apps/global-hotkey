0d9ea8a39a13d484956b717a9f94a41687d5e600
sb123sb123 <152394158+sb123sb123@users.noreply.github.com>
2026-09-21 22:15:20 +0800

    fix(error): align hotkey parse error type (#163)

--- src/error.rs
+++ src/error.rs
@@ -4,7 +4,7 @@
 
 use thiserror::Error;
 
-use crate::hotkey::HotKey;
+use crate::hotkey::{HotKey, HotKeyParseError};
 
 /// Errors returned by tray-icon.
 #[non_exhaustive]
@@ -13,13 +13,7 @@ pub enum Error {
     #[error(transparent)]
     OsError(#[from] std::io::Error),
     #[error("{0}")]
-    HotKeyParseError(String),
-    #[error("Couldn't recognize \"{0}\" as a valid HotKey Code, if you feel like it should be, please report this to https://github.com/tauri-apps/global-hotkey")]
-    UnrecognizedHotKeyCode(String),
-    #[error("Unexpected empty token while parsing hotkey: \"{0}\"")]
-    EmptyHotKeyToken(String),
-    #[error("Unexpected hotkey string format: \"{0}\", a hotkey should have the modifiers first and only contain one main key")]
-    UnexpectedHotKeyFormat(String),
+    HotKeyParseError(HotKeyParseError),
     #[error("Unable to register hotkey: {0}")]
     FailedToRegister(String),
     #[error("Failed to unregister hotkey: {0:?}")]
@@ -32,3 +26,19 @@ pub enum Error {
 
 /// Convenient type alias of Result type for tray-icon.
 pub type Result<T> = std::result::Result<T, Error>;
+
+#[cfg(test)]
+mod tests {
+    use super::Error;
+    use crate::hotkey::HotKeyParseError;
+
+    #[test]
+    fn hotkey_parse_error_uses_typed_error() {
+        let error = Error::HotKeyParseError(HotKeyParseError::EmptyToken("Shift++KeyA".into()));
+
+        assert_eq!(
+            error.to_string(),
+            "Found empty token while parsing hotkey: Shift++KeyA"
+        );
+    }
+}
